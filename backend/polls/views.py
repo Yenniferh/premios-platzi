009f6ddbcdc426e75f71ffdeee7c3d32025588c3
Yennifer Herrera <yenniferh@uninorte.edu.co>
2022-05-16 17:39:09 -0500

    ADD: App Views

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-  return HttpResponse('Hello World')
+  return HttpResponse('You are at the polls index.')
+
+
+def detail(request, question_id):
+  return HttpResponse(f'You are looking at question {question_id}.')
+
+
+def results(request, question_id):
+  return HttpResponse(f'You are looking at the results of question {question_id}.')
+
+
+def vote(request, question_id):
+  return HttpResponse(f'You are voting on question {question_id}.')
