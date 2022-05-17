d016db5d775d60a32cb48b866eb439360360b410
Yennifer Herrera <yenniferh@uninorte.edu.co>
2022-05-17 18:42:04 -0500

    ADD: Detail and Results templates

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
   latest_question_list = Question.objects.all()
@@ -18,8 +17,22 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-  return HttpResponse(f'You are looking at the results of question {question_id}.')
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/results.html', {
+    'question': question
+  })
 
 
 def vote(request, question_id):
-  return HttpResponse(f'You are voting on question {question_id}.')
+  question = get_object_or_404(Question, pk=question_id)
+  try:
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    return render(request, 'polls/detail.html', {
+      'question': question,
+      'error_message': "You didn't select a choice."
+    })
+
+  selected_choice.votes += 1
+  selected_choice.save()
+  return redirect('polls:results', question_id=question.id)
